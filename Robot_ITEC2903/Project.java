1d2c06b8afcfcf3e8a143904511b5e091963bf85
Gucci Boi <alex.lopez-corral@go.minneapolis.edu>
2024-09-06 14:08:42 -0500

    Changed Mouachee's code into the style we did in the Java class.

--- Robot_ITEC2903/Project.java
+++ Robot_ITEC2903/Project.java
@@ -4,27 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Project {
-    public static void main (String[] args) {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        frame.setSize(420,420);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.add(panel);
-
-        panel.setLayout(null);
-
-        JLabel label = new JLabel("Users");
-        label.setBounds(10,20,80,25);
-        panel.add(label);
-
-        JTextField userText = new JTextField(20);
-        userText.setBounds(100,20,165,25);
-        panel.add(userText);
-
-
-
-
-        frame.setVisible(true);
+    public static void main(String[] args) {
+        CreateAccountGUI gui = new CreateAccountGUI();
     }
+
 }
