f1b3db0c35265c74f5d0f45030c6c54f5164f2ca
Mouachee <ineedfriends83@gmail.com>
2024-09-12 10:16:05 -0500

    Added email,password, confirm password to the GUI.

--- Robot_ITEC2903/Project.java
+++ Robot_ITEC2903/Project.java
@@ -3,8 +3,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Project {
 
+public class Project {
+//
 
     public static void main(String[] args) {
         CreateAccountGUI gui = new CreateAccountGUI();
