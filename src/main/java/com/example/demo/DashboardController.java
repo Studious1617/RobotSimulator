3b5bdf5c719f73ad19fe41506917ced1a2e2e4a8
Gucci Boi <alex.lopez-corral@go.minneapolis.edu>
2024-10-07 13:16:50 -0500

    Created the Ruleset fxml and controller.

--- src/main/java/com/example/demo/DashboardController.java
+++ src/main/java/com/example/demo/DashboardController.java
@@ -1,5 +1,44 @@
 package com.example.demo;
 
-public class DashboardController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
+public class DashboardController {
+    @FXML
+    public GridPane DashboardGridPane;
+    public Node leftHBox;
+    public Node rightHBox;
+
+    public Button LayoutsPageButton;
+    public Button RulesetsPageButton;
+    public Button reportsButton;
+    public Button createLayoutButton;
+
+    public Button editLayoutButton1;
+    public Button editLayoutButton2;
+    public Button editLayoutButton3;
+    public Button editLayoutButton4;
+    public Button editLayoutButton5;
+
+    public Button viewLayoutButton1;
+    public Button viewLayoutButton2;
+    public Button viewLayoutButton3;
+    public Button viewLayoutButton4;
+    public Button viewLayoutButton5;
+
+    public Button deleteLayoutButton1;
+    public Button deleteLayoutButton2;
+    public Button deleteLayoutButton3;
+    public Button deleteLayoutButton4;
+    public Button deleteLayoutButton5;
+
+    @FXML
+    public void onCreateNewLayoutClick(ActionEvent e) throws Exception {
+        //
+        RulesetsPageButton.setStyle("-fx-background-color: null");
+    }
 }
--- src/main/java/com/example/demo/FactoryLayoutController.java
+++ src/main/java/com/example/demo/FactoryLayoutController.java
@@ -1,29 +1,20 @@
 package com.example.demo;
 
-import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
-/**
- * This will be the main class
- */
-
-public class FactoryLayoutController extends Application {
-
-    // main class. runs the program
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class FactoryLayoutController {
     //test
-    @Override
-    public void start(Stage createAccountStage) throws Exception {
-        //
+    public void toBeDecided(ActionEvent event) throws IOException {
         Parent popUp = FXMLLoader.load(getClass().getResource("FactoryLayout.fxml"));
-
-        Scene FacAccountScene = new Scene(popUp);
-        createAccountStage.setScene(FacAccountScene);
-        createAccountStage.show();
+        Stage stageFour = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene sceneFour = new Scene(popUp);
+        stageFour.setScene(sceneFour);
+        stageFour.show();
     }
 }
