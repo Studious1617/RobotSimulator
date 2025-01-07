6b955a69a686f80716e19a3a846e4dd7d26e6051
Reyna Trostad <reynatrostad@gmail.com>
2025-01-07 16:11:00 -0600

    feature 1 password validation

--- src/main/java/com/example/demo/CreateUserAccountController.java
+++ src/main/java/com/example/demo/CreateUserAccountController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import java.io.IOException;
 
@@ -42,26 +44,45 @@ public class CreateUserAccountController {
         if (sqlConfiguration.checkUserInfo(fullName, userEmail, password)
                 && password.equals(confirmPassword)) {
 
-            if (sqlConfiguration.checkEmailFormat(userEmail)) {
-                // adds the user to the database
-                if (sqlConfiguration.addNewUser(fullName, userEmail, password)) {
-                    Parent dashPopUp = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
-                    // switch to Dashboard
-                    Stage stageThree = (Stage) ((Node) e.getSource()).getScene().getWindow();
-                    Scene sceneThree = new Scene(dashPopUp,1540,800);
-                    stageThree.setScene(sceneThree);
-                    stageThree.show();
+            // calls passwordValid to check if the password is too simple
+            if (passwordValid(password)) {
+                errorLabel1.setVisible(true);
+                errorLabel1.setText("Passwords must have uppercase, lowercase, number and symbol");
+            } else
+                if (sqlConfiguration.checkEmailFormat(userEmail)) {
+                    // adds the user to the database
+                    if (sqlConfiguration.addNewUser(fullName, userEmail, password)) {
+                        Parent dashPopUp = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+                        // switch to Dashboard
+                        Stage stageThree = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                        Scene sceneThree = new Scene(dashPopUp,1540,800);
+                        stageThree.setScene(sceneThree);
+                        stageThree.show();
+                    }
+                    System.out.println("Account created.");
+                    errorLabel1.setText("Account created.");
+                } else {
+                    System.out.println("Invalid email address. Try again.");
+                    errorLabel1.setText("Invalid email address. Try again.");
                 }
-                System.out.println("Account created.");
-                errorLabel1.setText("Account created.");
             } else {
-                System.out.println("Invalid email address. Try again.");
-                errorLabel1.setText("Invalid email address. Try again.");
+                System.out.println("Invalid credentials. Try again.");
+                errorLabel1.setText("Invalid credentials. Try again.");
             }
-        } else {
-            System.out.println("Invalid credentials. Try again.");
-            errorLabel1.setText("Invalid credentials. Try again.");
-        }
+    }
+
+    public static boolean passwordValid(String password) {
+        // This regex matches with bad passwords
+        String simplePassword = "^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$";
+
+        // creates a pattern object
+        Pattern pattern =   Pattern.compile(simplePassword);
+
+        // creates a matcher object
+        Matcher matcher = pattern.matcher(password);
+
+        // returns if the password matches the regex or not
+        return matcher.matches();
     }
 
     // event for log in button
